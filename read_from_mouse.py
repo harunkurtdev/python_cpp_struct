244b2aa3d4bc61c0652db960254e87401a64f3ee
harunkurt.me <harunlakodla@gmail.com>
2022-09-09 00:56:39 +0300

    add communcation with mouse with file

--- read_from_mouse.py
+++ read_from_mouse.py
@@ -1,8 +1,17 @@
-mouse=open('/dev/input/mice')
 
+import struct
+
+
+mouse=open('/dev/input/mice',"rb") #read byte
 
 while True:
     
     event=mouse.read(3)
-
-    print(event)
+    # print(event)
+    
+    x,y=struct.unpack_from("bb",event[1:])# from byte read
+    #currently mouse x and y position
+    
+    print(x,y)
+    
+mouse.close()
